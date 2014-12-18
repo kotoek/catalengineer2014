633fc5f149367e87b23e4533e9ae2cb6ae3c4df7
kotoek <kotoe.kuro@gmail.com>
2014-12-18 11:12:57 +0900

    kouza kadai 3

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,27 @@
+class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+  end
+
+  def show
+     @group = Group.find(params[:id])
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+       redirect_to @group
+    else
+      render :new
+    end
+  end
+
+  def group_params
+     params.require(:group).permit(:name)
+  end
+
+  def index 
+     @groups = Group.all
+  end
+ 
+end
